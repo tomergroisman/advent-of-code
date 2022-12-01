dc4b5dfa2b2d8f265eb70df44bb54a97d6150a83
Tomer Groisman <tomergroisman@gmail.com>
2022-12-01 20:25:59 +0200

    (Solved) Day 6 / 2021

--- aoc2021/README.md
+++ aoc2021/README.md
@@ -16,7 +16,7 @@
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
-![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
@@ -104,9 +104,9 @@ Both parts: 210.25662499999999ms
 
 ```
 Day 06
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 20.555ms
+Time part 2: 0.102ms
+Both parts: 20.656426ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 10/50
-Total time: 300.964ms
+Total stars: 12/50
+Total time: 321.621ms
 ```
 
 <!--/RESULTS-->
---
+++ aoc2021/src/day06/index.ts
@@ -0,0 +1,73 @@
+import run from 'aocrunner';
+
+import { number } from '../utils/index.js';
+
+const parseInput = (rawInput: string) => rawInput.split(',').map(parseFloat);
+
+const simulate = (cycles: number[], days: number = 80) => {
+  const simulateDay = (cycles: number[]) => {
+    const existingFish: number = cycles.length;
+    for (let i = 0; i < existingFish; i++) {
+      const currentFishCycle: number = cycles[i]--;
+      if (currentFishCycle == 0) {
+        cycles[i] = 6;
+        cycles.push(8);
+      }
+    }
+  };
+
+  for (let i = 0; i < days; i++) {
+    simulateDay(cycles);
+  }
+};
+
+const calculateFishCount = (cycles: number[], days: number = 80): number => {
+  const fishCycleCount = new Array(9).fill(0);
+  for (const fishCycle of cycles) {
+    fishCycleCount[fishCycle]++;
+  }
+
+  for (let i = 0; i < days; i++) {
+    const fishToDouble = fishCycleCount.shift();
+    fishCycleCount.push(fishToDouble);
+    fishCycleCount[6] += fishToDouble;
+  }
+
+  return number.sumArray(fishCycleCount);
+};
+
+const part1 = (rawInput: string) => {
+  let cycles = parseInput(rawInput);
+  simulate(cycles);
+
+  return cycles.length;
+};
+
+const part2 = (rawInput: string) => {
+  let cycles = parseInput(rawInput);
+
+  return calculateFishCount(cycles, 256);
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `3,4,3,1,2`,
+        expected: 5934,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `3,4,3,1,2`,
+        expected: 26984457539,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
